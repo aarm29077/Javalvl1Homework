4809b375ca98c45876ad24a10662a633c2e09d7f
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2021-12-31 21:08:55 +0400

    Homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        long max = max(15, 98);
+        long a=25;
+        long b=145;
+        long c=614;
+        long max1;
+        long max2;
+        long max3;
+        if (a>max) {
+            max1=a;
+        }else{
+            max1 = max(15,98);
+        }
+        if (b>max1){
+            max2 = b;
+        }else {
+            max2 = max1;
+        }
+        if (c>max2){
+            max3 = c;
+        }else{
+            max3=max2;
+        }
+        System.out.println(max3);
+    }
+
+    public static long max(long i1, long i2) {
+        if (i1 > i2) {
+            return i1;
+        } else {
+            return i2;
+        }
+    }
+}
+
+
+
---
+++ src/homework2/FirstQuestion.java
@@ -0,0 +1,26 @@
+package homework2;
+
+public class FirstQuestion {
+    public static void main(String[] args) {
+    int max = max(25987,598,1455);
+        System.out.println(max);
+    }
+
+
+
+    public static int max(int v1, int v2, int v3) {
+        if (v1 > v2) {
+            if (v1 > v3) {
+                return v1;
+            } else {
+                return v3;
+            }
+        } else {
+            if (v2 > v3) {
+                return v2;
+            } else {
+                return v3;
+            }
+        }
+    }
+}
