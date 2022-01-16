af93fb9ad2377dd9e3eac638e96d34fd51206ef8
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-01-17 02:54:52 +0400

    My homework

--- src/homework3/EightQuestion.java
+++ src/homework3/EightQuestion.java
@@ -3,37 +3,36 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class EightQuestion {
     public static void main(String[] args) {
-        System.out.print("0 ");
-        for (int i = 0; i <= 9 ; i++) {
-            System.out.print(i +" ");
+        for (int i = 0; i <= 16 ; i++) {
+            System.out.print(i +"  ");
+            if (i>=9){
+                System.out.print("");
+            }
         }
         System.out.println();
-        int linenumber=0;
-        String board[][] = new String[10][10];
+        int linenumber=1;
+        String board[][] = new String[16][16];
         for (int i = 0; i < board.length; i++) {
             System.out.print(linenumber + " ");
+            if (i<9){
+                System.out.print(" ");
+            }
             for (int j = 0; j < board[i].length; j++) {
-                board[i][j] = "X";
+                if (j>=9){
+                    System.out.print(" ");
+                }
+                board[i][j] = "* ";
                 System.out.print(board[i][j] + " ");
-                ;
+
             }
             ++linenumber;
             System.out.println("");
+            System.out.println(" ");
         }
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Input line number where you want to put it: ");
-//        int a=scanner.nextInt();
-//        System.out.println("Input column number where you want to put it: ");
-//        int b= scanner.nextInt();
-//
-//        System.out.println("Start!!!!!! ");
-//        System.out.println("Input the number of linet shoot : ");
-//        int shoot1 = scanner .nextInt();
-//        System.out.println("Input number of column to shoot: ");
-//        int shoot2= scanner.nextInt();
 
     }
+
 }
 
 
--- src/homework3/FirstQuestion.java
+++ src/homework3/FirstQuestion.java
@@ -2,17 +2,7 @@ package homework3;
 import java.util.Scanner;
 public class FirstQuestion {
     public static void main(String[] args) {
-        String passportName = "John";
-        int passportAge = 43;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input age: ");
-        int age = scanner.nextInt();
-        if (isAnsweredNameSameWithPassportName(passportName) && isUserAgeTheSameAsPassportAge(passportAge, age) && isUserAgeGreaterThan18(age)) {
-            System.out.println("YES");
-        }else{
-            System.out.println("No");
-        }
-
+        System.out.println(program());
     }
     public static boolean isAnsweredNameSameWithPassportName(String passportName){
         Scanner scanner = new Scanner(System.in);
@@ -25,6 +15,15 @@ public class FirstQuestion {
     }
     public static boolean isUserAgeGreaterThan18(int age){
         return age>18;
+    }
+    public static String program(){
+        String passportName = "John";
+        int passportAge = 43;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input age: ");
+        int age = scanner.nextInt();
+        return (isAnsweredNameSameWithPassportName(passportName) && isUserAgeTheSameAsPassportAge(passportAge, age) && isUserAgeGreaterThan18(age)) ? "YES" : "No";
+
     }
 
 }
