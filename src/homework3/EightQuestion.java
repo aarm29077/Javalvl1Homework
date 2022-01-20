5a57abf6b700320de3fc07b41486a0e4ee6eff09
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-01-20 16:08:14 +0400

    Homework3

--- src/homework3/EightQuestion.java
+++ src/homework3/EightQuestion.java
@@ -3,35 +3,27 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class EightQuestion {
     public static void main(String[] args) {
-        for (int i = 0; i <= 16 ; i++) {
-            System.out.print(i +"  ");
-            if (i>=9){
-                System.out.print("");
-            }
+        System.out.print(" ");
+        for (int i = 0; i <10 ; i++) {
+            System.out.print("  " + i );
         }
         System.out.println();
-        int linenumber=1;
-        String board[][] = new String[16][16];
+        int linenumber=0;
+        String board[][] = new String[10][10];
         for (int i = 0; i < board.length; i++) {
-            System.out.print(linenumber + " ");
-            if (i<9){
-                System.out.print(" ");
-            }
+            System.out.print(linenumber + "  ");
             for (int j = 0; j < board[i].length; j++) {
-                if (j>=9){
-                    System.out.print(" ");
-                }
                 board[i][j] = "* ";
                 System.out.print(board[i][j] + " ");
-
             }
             ++linenumber;
-            System.out.println("");
             System.out.println(" ");
         }
 
 
     }
+
+
 
 }
 
---
+++ src/homework4/ChessBoard.java
@@ -0,0 +1,48 @@
+package homework4;
+import java.util.Scanner;
+public class ChessBoard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char array[][] = new char[8][8];
+        int linenumber=8;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(linenumber+" ");
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j]='#';
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println("");
+            linenumber--;
+        }
+        System.out.print("  " + "a "+ "b "+"c " + "d "+ "e " + "f " + "g "+"h ");
+        System.out.println();
+        System.out.println();
+
+        System.out.print("Input place letter and number: ");
+        String place=scanner.next();
+        char placeLetter = place.charAt(0) ;
+        char placeNumber=place.charAt(1);
+        System.out.print("Input place letter and number to move: ");
+        String placeToMove = scanner.next();
+        char placeLetterToMove=placeToMove.charAt(0);
+        char placeNumberToMove=placeToMove.charAt(1);
+        horse(array,placeLetter,placeNumber,placeLetterToMove,placeNumberToMove);
+
+////       horse(array, 4,1,5,3);
+    }
+
+    public static void horse(char[][] array1, char i, char j , char k , char l){
+        if ((i<=104 && i>=97 && k<=104 && k>=97 && j>=48 && j<=56 && l>=48 && l<=56)){
+            if ((i-k == 1 || k-i==1) && (j-l== 2 || l-j==2)){
+            System.out.println("Yes");
+        }
+            else if ((i-k == 2 || k-i==2) && (j-l== 1 || l-j==1)){
+            System.out.println("Yes");
+    }
+            else{
+            System.out.println("No");
+        }
+}else {
+            System.out.println("Error");
+        }
+    }}
