a7120e4b5bc6fa4df206b1143258200b12c743cb
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-01-24 01:29:54 +0400

    User Homework

---
+++ src/com/asa/homework/week2/symbolcount/SymbolCount.java
@@ -0,0 +1,20 @@
+package com.asa.homework.week2.symbolcount;
+
+import java.util.Scanner;
+
+public class SymbolCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input text: ");
+        String word =  scanner.nextLine();
+        System.out.print("Please write a symbol: ");
+        String letter = scanner.nextLine();
+        int sum=0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i)==letter.charAt(0)){
+                sum+=1;
+            }
+        }
+        System.out.println(sum);
+    }
+}
