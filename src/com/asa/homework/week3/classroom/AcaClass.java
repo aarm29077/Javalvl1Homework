692cc3f5c918507458f4e8b5f5dc6b3a771263f6
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-01-27 18:27:01 +0400

    User Homework

--- src/com/asa/homework/week3/classroom/AcaClass.java
+++ src/com/asa/homework/week3/classroom/AcaClass.java
@@ -2,9 +2,13 @@ package com.asa.homework.week3.classroom;
 
 public class AcaClass {
     Student student[];
+
     long startMillis;
+
     long endMillis;
+
     String name;
+
     int fee;
 
 }
--- src/com/asa/homework/week3/classroom/HomeworkTest.java
+++ src/com/asa/homework/week3/classroom/HomeworkTest.java
@@ -11,6 +11,7 @@ public class HomeworkTest {
 
 
         Student student1 = new Student();
+
         student1.name = "Arman";
         student1.surname = "Vardanyan";
         Student student2 = new Student();
