5a423fdd092b345639f4eb7bda377d4de99dff7b
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-07 13:19:37 +0400

    The solution of 'numbers: max  and min'

---
+++ src/com/aca/homework/week3/numbers/maxMin/Test.java
@@ -0,0 +1,9 @@
+package com.aca.homework.week3.numbers.maxMin;
+
+public class Test {
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers(3,4,5);
+        System.out.println(numbers.max());
+        System.out.println(numbers.min());
+    }
+}
