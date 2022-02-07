f65149ee30151da8206a29b3435948a6bf86fad5
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-07 13:27:55 +0400

    Homework

---
+++ src/com/aca/homework/week4/developer/JuniorDeveloper.java
@@ -0,0 +1,11 @@
+package com.aca.homework.week4.developer;
+
+public class JuniorDeveloper extends Developer {
+
+    public int getLearningBudget() {
+        return 600;
+    }
+    public int getSalary(){
+        return 300_000;
+    }
+}
---
+++ src/com/aca/homework/week4/developer/SeniorDeveloper.java
@@ -0,0 +1,20 @@
+package com.aca.homework.week4.developer;
+/*
+Do not use a constructor other than a default in the above-mentioned classes.
+Create a print method to print the details of the class.
+Create a developer named Gerard and print it (the print text should be "name: Gerard, salary: 60000, learning budget: 300")
+Create a junior developer named Maricar and print it. (the print text should be "name: Maricar, salary: 300000, learning budget: 600")
+Create a senior developer named Emily and print it. (the print text should be "name: Emily, salary: 3000000, learning budget: 2400")
+note: The Developer class should have only one name field. The JuniorFeveloper and the SeniorDeveloper should not have any field.
+ */
+public class SeniorDeveloper extends Developer {
+
+    public int getLearningBudget() {
+        return 2_400;
+    }
+
+    public int getSalary(){
+        return 3_000_000;
+    }
+
+}
