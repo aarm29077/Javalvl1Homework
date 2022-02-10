d4ccc53879af7ac79a6845a587cfc6edba18057f
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-10 11:49:13 +0400

    Image

--- src/com/aca/homework/week4/image/Image.java
+++ src/com/aca/homework/week4/image/Image.java
@@ -2,35 +2,37 @@ package com.aca.homework.week4.image;
 
 public class Image {
 
-     int verticalLength ;
-     int horizontalLength;
+    int verticalLength;
+    int horizontalLength;
+    char[][] pixel;
 
-    public Image(int verticalLength,int horizontalLength){
-        this.verticalLength=verticalLength;
-        this.horizontalLength=horizontalLength;
-
+    public Image(int verticalLength, int horizontalLength) {
+        this.verticalLength = verticalLength;
+        this.horizontalLength = horizontalLength;
+        this.pixel = new char[verticalLength][horizontalLength];
+        fill();
     }
-    char[][] pixel = new char[verticalLength][horizontalLength];
 
-    public void input(){
+    public void fill() {
         for (int i = 0; i < pixel.length; i++) {
-            for (int j = 0; j < pixel[verticalLength].length; j++) {
+            for (int j = 0; j < pixel[i].length; j++) {
                 pixel[i][j] = '-';
             }
         }
     }
 
-    public void show(){
+    public void show() {
         for (int i = 0; i < pixel.length; i++) {
-            for (int j = 0; j < pixel[verticalLength].length; j++) {
-                System.out.println(pixel[i][j] + " ");
+            for (int j = 0; j < pixel[i].length; j++) {
+                System.out.print(pixel[i][j] + " ");
             }
             System.out.println();
         }
 
     }
-    public void draw(int verticalIndex , int horizontalIndex){
-        pixel[verticalIndex][horizontalIndex]='*';
+
+    public void draw(int verticalIndex, int horizontalIndex) {
+        pixel[verticalIndex][horizontalIndex] = '*';
 
     }
 
