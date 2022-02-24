fe54781f5fe760aadf1783a66ee934b17b14580e
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-24 18:03:24 +0400

    week5/exam

--- src/com/aca/homework/week5/exam/FailedExamTest.java
+++ src/com/aca/homework/week5/exam/FailedExamTest.java
@@ -3,6 +3,6 @@ package com.aca.homework.week5.exam;
 public class FailedExamTest extends Exam {
     public FailedExamTest(String username) {
         super(username);
-        this.passed=false;
+        this.passed = false;
     }
 }
--- src/com/aca/homework/week5/exam/PassedExamTest.java
+++ src/com/aca/homework/week5/exam/PassedExamTest.java
@@ -3,6 +3,6 @@ package com.aca.homework.week5.exam;
 public class PassedExamTest extends Exam {
     public PassedExamTest(String username) {
         super(username);
-        this.passed=true;
+        this.passed = true;
     }
 }
