6334c750f5b62f0dd83a45b17f73e14de5a0f928
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-24 15:24:26 +0400

    The solution of the 'car tax' problem

---
+++ src/main/com/aca/homework/week7/car/tax/Type.java
@@ -0,0 +1,24 @@
+package main.com.aca.homework.week7.car.tax;
+
+/*
+* There are 2 type of cars: ELECTRIC and HYBRID.
+The annual vehicle tax is 5$ regardless of the year if the car type is electric and 5$ x carAge if the car type is hybrid.
+Create CarType enum class and implement the annualTex method.
+*
+*ստեղծում եք նաև CarTypeTest  class test folder-ի com.aca.homework.week7.car.tax package-ում (ctrl+shift+T),
+* հաշվում եք հիբրիդի համար տարեկան հարկը, եթե մեքենան արտադրվել է 2010 թվականին։ Նույն կերպ էլեկտրական մեքենայի համար, արտադրված 2015 թվականին։
+*
+* */
+public enum Type {
+    ELECTRIC,
+    HYBRID;
+
+    public int annualTaxOfElectric(int carYear) {
+        int carAge = 2022 - carYear + 1;
+        if (this == ELECTRIC) {
+            return carAge * 5;
+        } else {
+            return ((2 * 5 + ((carAge - 1) * 5)) * carAge) / 2;
+        }
+    }
+}
---
+++ src/text/main/com/aca/homework/week7/car/tax/TypeTest.java
@@ -0,0 +1,17 @@
+package main.com.aca.homework.week7.car.tax;
+
+/*
+* There are 2 type of cars: ELECTRIC and HYBRID.
+The annual vehicle tax is 5$ regardless of the year if the car type is electric and 5$ x carAge if the car type is hybrid.
+Create CarType enum class and implement the annualTex method.
+*
+*ստեղծում եք նաև CarTypeTest  class test folder-ի com.aca.homework.week7.car.tax package-ում (ctrl+shift+T),
+* հաշվում եք հիբրիդի համար տարեկան հարկը, եթե մեքենան արտադրվել է 2010 թվականին։ Նույն կերպ էլեկտրական մեքենայի համար, արտադրված 2015 թվականին։
+*
+* */
+
+class TypeTest {
+    public static void main(String[] args) {
+        System.out.println(Type.HYBRID.annualTaxOfElectric(2010));
+    }
+}
