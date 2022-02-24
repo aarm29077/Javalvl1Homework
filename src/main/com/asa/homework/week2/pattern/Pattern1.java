8384c6aacd5df14361da5b8c7e67655843a8bad3
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-24 12:47:38 +0400

    homework

---
+++ src/main/com/asa/homework/week2/pattern/Pattern1.java
@@ -0,0 +1,4 @@
+package com.asa.homework.week2.pattern;
+
+public class Pattern1 {
+}
---
+++ src/main/homework3/SecondQuestion.java
@@ -0,0 +1,24 @@
+package homework3;
+import java.util.Scanner;
+public class SecondQuestion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a: ");
+        int a =scanner.nextInt();
+        System.out.print("Input b: ");
+        int b =scanner.nextInt();
+        System.out.print("Input c: ");
+        int c =scanner.nextInt();
+        System.out.print("Input d: ");
+        int d =scanner.nextInt();
+        System.out.print("Input e: ");
+        int e =scanner.nextInt();
+        System.out.print("Input f: ");
+        int f = scanner.nextInt();
+    if (a==10 || b==10 || c==10 || d==10 || e==10 || f==10){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+    }
+}
