12cc1a2e685fbbbfe33ece9b0c80aa3cdfa5c0b8
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-02-25 11:48:27 +0400

    The solution of the 'additional number' problem

---
+++ src/text/main/com/aca/homework/week7/additional/number/AdditionalNumberTest.java
@@ -0,0 +1,11 @@
+package main.com.aca.homework.week7.additional.number;
+
+
+class AdditionalNumberTest {
+    public static void main(String[] args) {
+        int[] firstArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int[] secondArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        System.out.println(Numbers.findNumber(firstArray, secondArray));
+    }
+
+}
