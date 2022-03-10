8e78c8a422a201619b3c79c2888a29280b3c86b9
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-03-10 18:01:28 +0400

    week7/singleton

---
+++ src/main/com/aca/homework/week7/singleton/SingletonUserService.java
@@ -0,0 +1,22 @@
+package main.com.aca.homework.week7.singleton;
+/*
+* Create the SingletonUserService class that has a private constructor. Create a static factory getInstance() method
+* that returns an object of the SingletonUserService if it is already created, otherwise creates and returns it.
+*
+* ստեղծում եք նաև SingletonUserServiceTest  class test folder-ի com.aca.homework.week7.singleton package-ում (ctrl+shift+T),
+* main method-ի մեջ ստեղծում եք 4 object getInstance()-ի միջոցով, համեմատում դրանք ==֊ի միջոցով, տպում և տեսնում եք որ true է տպվել
+* */
+public class SingletonUserService {
+
+    public static SingletonUserService singletonUserService = new SingletonUserService();
+
+    private SingletonUserService(){
+
+    }
+    public static SingletonUserService getInstance(){
+        if (singletonUserService==null){
+            singletonUserService=new SingletonUserService();
+        }
+        return  singletonUserService;
+    }
+}
