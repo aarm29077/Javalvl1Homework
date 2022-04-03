e92ee9aa3d17e0937020c1ad96c5fec240e4d43f
aarm29077 <90210840+aarm29077@users.noreply.github.com>
2022-04-03 22:40:50 +0400

    week12/create-update-params

--- src/test/com/aca/homework/week12/params/UserCreateParamsTest.java
+++ src/test/com/aca/homework/week12/params/UserCreateParamsTest.java
@@ -7,51 +7,53 @@ import org.junit.jupiter.api.function.Executable;
 class UserCreateParamsTest {
 
     @Test
-    void testAge(){
+    void testAge() {
         Assertions.assertThrows(RuntimeException.class, new Executable() {
             @Override
             public void execute() throws Throwable {
-                new UserCreateParams(new UserUpdateParams("Hayk001","Hayk",0));
+                new UserCreateParams(new UserUpdateParams("Hayk001", "Hayk", 0));
             }
         });
     }
 
     @Test
-    void testUsername(){
+    void testUsername() {
         Assertions.assertThrows(RuntimeException.class, new Executable() {
             @Override
             public void execute() throws Throwable {
-                new UserCreateParams(new UserUpdateParams(null,"Hayk",22));
+                new UserCreateParams(new UserUpdateParams(null, "Hayk", 22));
             }
         });
     }
+
     @Test
-    void testFirstName(){
+    void testFirstName() {
         Assertions.assertThrows(RuntimeException.class, new Executable() {
             @Override
             public void execute() throws Throwable {
-                new UserCreateParams(new UserUpdateParams("Hayk001",null,22));
+                new UserCreateParams(new UserUpdateParams("Hayk001", null, 22));
             }
         });
     }
+
     @Test
-    void test(){
+    void test() {
         Assertions.assertDoesNotThrow(new Executable() {
             @Override
             public void execute() throws Throwable {
-                new UserCreateParams(new UserUpdateParams("Hayk001","Hayk",22));
+                new UserCreateParams(new UserUpdateParams("Hayk001", "Hayk", 22));
             }
         }, "The age is inadmissible");
         Assertions.assertDoesNotThrow(new Executable() {
             @Override
             public void execute() throws Throwable {
-                new UserCreateParams(new UserUpdateParams("Hayk001","Hayk",22));
+                new UserCreateParams(new UserUpdateParams("Hayk001", "Hayk", 22));
             }
         }, "The username is null!!");
         Assertions.assertDoesNotThrow(new Executable() {
             @Override
             public void execute() throws Throwable {
-                new UserCreateParams(new UserUpdateParams("Hayk001","Hayk",22));
+                new UserCreateParams(new UserUpdateParams("Hayk001", "Hayk", 22));
             }
         }, "The firstName is null!!");
     }
